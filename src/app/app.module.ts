278bb18d5f43f5bad0978dce2b033f5b29f27315
Hakan GÜZEL <hakan-guzel@outlook.com>
2018-02-27 23:36:55 +0300

    ilk proje

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { CartComponent } from './cart/cart.component';
+import { AccountComponent } from './account/account.component';
+import { ShippingDetailComponent } from './shipping-detail/shipping-detail.component';
+import { CartSummaryComponent } from './cart/cart-summary/cart-summary.component';
+import { LoggedComponent } from './account/logged/logged.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    CartComponent,
+    AccountComponent,
+    ShippingDetailComponent,
+    CartSummaryComponent,
+    LoggedComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule
